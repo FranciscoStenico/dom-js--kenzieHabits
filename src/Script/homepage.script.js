ab1cf041517344312f46e8261240f2130310cd02
Francisco Stenico <francisco.stenico@gmail.com>
2022-07-13 19:03:36 -0300

    Hotfix/realocação: função menuDropDown transformada em método na render.models.js

--- src/Script/homepage.script.js
+++ src/Script/homepage.script.js
@@ -6,22 +6,4 @@ setTimeout(() => {
 	Request.listHabits(10);
 }, 100);
 
-
-function menuDropDownDOM() {
-	const getMenuContainer = document.querySelector(".dropdownMenu__container");
-	const createDivContainer = document.createElement("div");
-	createDivContainer.classList = "dropdown";
-	const figureDropDown = document.createElement("figure");
-	figureDropDown.classList = "figureDropDown menu__container__foto";
-	const createUlList = document.createElement("ul");
-	createUlList.classList = "dropdown-content";
-	const createLiEditarPerfil = document.createElement("li");
-	createLiEditarPerfil.innerText = "Editar Perfil";
-	const createLiLogout = document.createElement("li");
-	createLiLogout.innerText = "Sair"
-
-	createUlList.append(createLiEditarPerfil, createLiLogout)
-	createDivContainer.append(figureDropDown, createUlList)
-	getMenuContainer.append(createDivContainer)
-
-}menuDropDownDOM()
+Render.menuDropDown()
