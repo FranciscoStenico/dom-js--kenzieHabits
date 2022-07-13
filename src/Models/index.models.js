f1787d9b175fc78a6b98c751221eed1cdaa233b7
Pedro <pedrosandes2010@gmail.com>
2022-07-12 21:07:53 -0300

    atualização de index

---
+++ src/Models/index.models.js
@@ -0,0 +1,28 @@
+import LoginRequest from "../Controllers/index.cotroler.js";
+
+const botaoLogin = document.getElementById("btnLogin")
+
+class CaptarLogin{
+    static dados(event){
+        event.preventDefault();
+
+        const formDeLogin = document.querySelector(".formDosInputs")
+        const data = {}
+        
+        for(let i = 0; i < formDeLogin.length; i++){
+            const {name, value} = formDeLogin[i];
+            if(name){
+                if(value === ""){
+                    return alert("Digite os dados de login")
+                }
+                data[name] = value
+            }
+        }
+        return data
+    }
+}
+    
+    botaoLogin.addEventListener("click", async(event) => {
+        const dadosDoLogin = CaptarLogin.dados(event)
+        LoginRequest.login(dadosDoLogin)
+    })
