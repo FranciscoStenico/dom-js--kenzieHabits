7b920fe852e0cc74b1a56ad1bad100d139cd826c
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2022-07-14 01:09:58 -0300

    Padronização dos modais

--- src/Models/modal.models.js
+++ src/Models/modal.models.js
@@ -1,4 +1,23 @@
 // Codar aqui os modais!!
 export default class Modal {
-    
+
+    static modalScreen = document.querySelector(".modal-screen")
+    static formHeader = document.querySelector(".form-header")
+
+    static createHabit(){
+
+    }
+
+    static editHabit(){
+
+    }
+
+    static editProfile(){
+
+    }
+
+    static deleteHabit(){
+
+    }
+
 }
